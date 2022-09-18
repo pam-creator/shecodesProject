4987d26327ba829d2f0a7d021288c532534e03a7
pam-creator <112515320+pam-creator@users.noreply.github.com>
2022-09-18 23:49:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -22,24 +22,59 @@ currentDate.innerHTML = `${currentDay}, ${hours}:${minutes}`;
 function showTemperature(response) {
     let temperature = document.getElementById("temperature");
     let temp = Math.round(response.data.main.temp);
-    temperature.innerHTML = `${temp}˚`;
-  };
-
-function search(event){
-    event.preventDefault();
-    let searchInput = document.querySelector('#search-text-input');
+    let descriptionElement = document.getElementById("weather-description");
+    let humidityElement = document.getElementById("humidity");
+    let windElement = document.getElementById("wind");
     let city = document.querySelector('#city');
-    city.innerHTML = `${searchInput.value}`;
+    
+    
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humidity;
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+    temperature.innerHTML = `${temp}`;
+    city.innerHTML = response.data.name;
+};
+
+function search(city){
     let apiKey = "e8690fee550e3cce67ea38c5d2186819";
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&units=metric&appid=${apiKey}`;
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
     axios.get(url).then(showTemperature);
 };
-let form = document.querySelector('#searchbar');
 
-form.addEventListener("submit", search);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("search-text-input");
+  search(cityInputElement.value);
+};
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
 
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheiTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheiTemperature);
+}
 
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
 
+let celsiusTemperature = null;
 
+let form = document.querySelector("#searchbar");
+form.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+search("Oslo");
 
 
