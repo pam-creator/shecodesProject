cbb6764b1cdc42a2c011be29acfb07440c0a1b9c
pam-creator <112515320+pam-creator@users.noreply.github.com>
2022-09-25 20:39:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,48 @@
 
 currentDate.innerHTML = `${currentDay}, ${hours}:${minutes}`;
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+};
+
+function displayForecast(response){
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+  forecastHTML = forecastHTML +`
+  <div class="col-2">
+            <div class="weather-forecast-day">${formatDay(forecastDay.dt)}</div>
+            <img
+               src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" 
+               alt=""
+               width="42"
+            />
+            <div class="weather-forecast-temperatures">
+                <span class="max-temperature">${Math.round(forecastDay.temp.max)}˚</span>
+                <span class="min-temperature">${Math.round(forecastDay.temp.min)}˚</span>
+            </div>
+         </div>`;
+  }
+});
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+
+};
+
+function getForecast(coordinates) {
+  let apiKey = "e0a5a97de9a0b7a951e9d154a8f9bad8";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+};
+
 function showTemperature(response) {
     let temperature = document.getElementById("temperature");
     let temp = Math.round(response.data.main.temp);
@@ -38,10 +80,12 @@ function showTemperature(response) {
     windElement.innerHTML = Math.round(response.data.wind.speed);
     temperature.innerHTML = `${temp}`;
     city.innerHTML = response.data.name;
+
+    getForecast(response.data.coord);
 };
 
 function search(city){
-    let apiKey = "e8690fee550e3cce67ea38c5d2186819";
+    let apiKey = "e0a5a97de9a0b7a951e9d154a8f9bad8";
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
     axios.get(url).then(showTemperature);
 };
@@ -51,15 +95,7 @@ function handleSubmit(event) {
   let cityInputElement = document.querySelector("#search-text-input");
   search(cityInputElement.value);
 };
-function displayFahrenheitTemperature(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
 
-  celsiusLink.classList.remove("active");
-  fahrenheitLink.classList.add("active");
-  let fahrenheiTemperature = (celsiusTemperature * 9) / 5 + 32;
-  temperatureElement.innerHTML = Math.round(fahrenheiTemperature);
-}
 
 function displayCelsiusTemperature(event) {
   event.preventDefault();
@@ -69,17 +105,10 @@ function displayCelsiusTemperature(event) {
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 
-let celsiusTemperature = null;
 
 let form = document.querySelector("#searchbar");
 form.addEventListener("submit", handleSubmit);
 
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
-
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Oslo");
-
 
